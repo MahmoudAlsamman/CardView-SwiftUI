3abde21b1f55d3160b8e91a284702640fdbb81a5
Mahmoud Alsamman <acc.ma7moud@gmail.com>
2021-05-31 19:42:24 +0200

    Initial Commit

---
+++ CardView-Example/CardView_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardView_ExampleApp.swift
+//  CardView-Example
+//
+//  Created by Mahmoud Alsamman on 31/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct CardView_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardView-Example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CardView-Example
+//
+//  Created by Mahmoud Alsamman on 31/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
